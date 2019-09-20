31e3aee33975396afad8147aa9f2e7f8dc31af2f
Nekzus <willianrazera@outlook.com>
2019-09-20 20:01:28 -0300

    2001

---
+++ Biblioteca/Projeto/Controllers/CadastroController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Projeto.Controllers
+{
+    public class CadastroController : Controller
+    {
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+    }
+}
