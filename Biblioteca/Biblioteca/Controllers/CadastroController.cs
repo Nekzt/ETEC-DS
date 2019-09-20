e39471457a745ac4613512ba1f14d138c00ad488
Nekzus <willianrazera@outlook.com>
2019-09-20 19:36:43 -0300

    mudanças menores

--- Biblioteca/Biblioteca/Controllers/CadastroController.cs
+++ Biblioteca/Biblioteca/Controllers/CadastroController.cs
@@ -1,93 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Projeto.Models;
 
-namespace Biblioteca.Controllers
+namespace Projeto.Controllers
 {
     public class CadastroController : Controller
     {
-        // GET: Cadastro
-        public ActionResult Index()
+        public IActionResult Cadastro()
         {
             return View();
-        }
-
-        // GET: Cadastro/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: Cadastro/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Cadastro/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Cadastro/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Cadastro/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Cadastro/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Cadastro/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
         }
     }
 }
