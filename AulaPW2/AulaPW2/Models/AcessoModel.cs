b8385e650628e0cae60de0a021bc53c30e5450be
Nekzus <willianrazera@outlook.com>
2019-10-09 20:41:57 -0300

    init

---
+++ AulaPW2/AulaPW2/Models/AcessoModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AulaPW2.Models
+{
+    public class AcessoModel
+    {
+        public string User
+        {
+            get
+            {
+                return User;
+            }
+            set
+            {
+                User = value;
+            }
+        }
+
+        public string Pass
+        {
+            get
+            {
+                return Pass;
+            }
+            set
+            {
+                Pass = value;
+            }
+        }
+
+
+    }
+}
