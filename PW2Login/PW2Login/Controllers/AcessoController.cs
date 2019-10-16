24171eea3952ef4f693612700758c9e37f73719c
maykep <maykepaiva.mgap@gmail.com>
2019-10-16 20:38:33 -0300

    Att.4

---
+++ PW2Login/PW2Login/Controllers/AcessoController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PW2Login.Controllers
+{
+    public class AcessoController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
