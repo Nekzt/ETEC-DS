41159396634c8401e2d6bb5bef6a5bf21df29570
maykep <maykepaiva.mgap@gmail.com>
2019-10-16 19:42:31 -0300

    teste

---
+++ PW2Login/PW2Login/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PW2Login.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
