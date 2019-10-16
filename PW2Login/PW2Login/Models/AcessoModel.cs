e40e2e0abed8906e368782f03f0a733d0254c246
maykep <maykepaiva.mgap@gmail.com>
2019-10-16 19:53:05 -0300

    Att.1

---
+++ PW2Login/PW2Login/Models/AcessoModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PW2Login.Models
+{
+    public class AcessoModel
+    {
+         public string Usuario
+        {
+            get;set;
+        }
+         public string Senha
+        {
+            get;set;
+        }
+    }
+}
