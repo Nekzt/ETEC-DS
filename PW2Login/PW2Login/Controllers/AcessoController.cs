2dc929de0ed58a7cf101df2318de7cfbf487d260
maykep <maykepaiva.mgap@gmail.com>
2019-10-17 19:41:29 -0300

    Att.6

--- PW2Login/PW2Login/Controllers/AcessoController.cs
+++ PW2Login/PW2Login/Controllers/AcessoController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PW2Login.Models;
 
 namespace PW2Login.Controllers
 {
@@ -11,6 +12,21 @@ namespace PW2Login.Controllers
         public IActionResult Login()
         {
             return View();
+
         }
+        public IActionResult Longin(AcessoModel acesso)
+        {
+            AutenticacaoAPIController autenticacaoAPI = new AutenticacaoAPIController();
+            if (autenticacaoAPI.ValidaLogin(acesso))
+                return RedirectToAction("Index", "Home");
+            else
+                return RedirectToAction("Login", "Acesso");
+        }
+
+        public IActionResult Logout()
+        {
+            return View();
+        }
+
     }
 }
